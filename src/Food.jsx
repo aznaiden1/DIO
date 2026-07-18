780ec668a0f5ac7d570c04062cfbda557adcc6e0
aznaiden1 <aznaiden1@gmail.com>
2026-07-18 16:28:59 -0700

    First commit

---
+++ src/Food.jsx
@@ -0,0 +1,13 @@
+
+function Food(){
+     const food1 = "Fried Chicken";
+    const food2 = "Smoked Chicken";
+    return (
+   <ul>
+    <li>Deez</li>
+    <li>{food1}</li>
+    <li>{food2}</li>    
+   </ul>
+    );
+}
+export default Food
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} Kono App. All rights reserved.
+            </p>
+        </footer>
+    );
+}
+export default Footer
